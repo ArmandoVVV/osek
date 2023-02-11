592022976f63e7b2485a1d5a0158956a37dbe690
ArmandoVVV <ie727115@iteso.mx>
2023-02-10 21:06:54 -0600

    funcional

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /**
  * @file    tarea1.c
  * @brief   Application entry point.
+ *  Author: Armando
  */
 #include "config.h"
 #include "osek.h"
@@ -12,26 +13,27 @@
 #define TASK_A_ID	0
 #define TASK_B_ID	1
 #define TASK_C_ID	2
+#define US_TO_DELAY	1000000
 
 void task_A_function(void){
 	green_led_on();
-	SDK_DelayAtLeastUs(1000000,21000000);
+	SDK_DelayAtLeastUs(US_TO_DELAY,CLOCK_GetFreq(kCLOCK_CoreSysClk));
 	activate_task(TASK_B_ID);
 	green_led_on();
-	SDK_DelayAtLeastUs(1000000,21000000);
+	SDK_DelayAtLeastUs(US_TO_DELAY,CLOCK_GetFreq(kCLOCK_CoreSysClk));
 	terminate_task();
 }
 
 void task_B_function(void){
 	red_led_on();
-	SDK_DelayAtLeastUs(1000000,21000000);
+	SDK_DelayAtLeastUs(US_TO_DELAY,CLOCK_GetFreq(kCLOCK_CoreSysClk));
 	chain_task(TASK_C_ID);
 	terminate_task();
 }
 
 void task_C_function(void){
 	blue_led_on();
-	SDK_DelayAtLeastUs(1000000,21000000);
+	SDK_DelayAtLeastUs(US_TO_DELAY,CLOCK_GetFreq(kCLOCK_CoreSysClk));
 	terminate_task();
 }
 
